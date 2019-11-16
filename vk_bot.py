254fa602282dd239f58d8041dfc5de5b8f836b56
Anatoly Rubanov <anatolijrubanov@gmail.com>
2019-11-16 19:38:52 +0300

    add authorization

--- vk_bot.py
+++ vk_bot.py
@@ -1,28 +1,25 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import requests
-import bs4
+from models import Base, User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-class VkBot:
+engine = create_engine('sqlite:///bot.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+token_list = ['123']
 
-    def __init__(self, user_id):
-        print("Создан объект бота!")
-        self._USER_ID = user_id
-        self._USERNAME = self._get_user_name_from_vk_id(user_id)
 
-        self._COMMANDS = ["ПРИВЕТ", "ПОГОДА", "ВРЕМЯ", "ПОКА"]
+def ml_part(query_string):
+    return "blablabla"
 
-    def _get_user_name_from_vk_id(self, user_id):
-        request = requests.get("https://vk.com/id" + str(user_id))
-        bs = bs4.BeautifulSoup(request.text, "html.parser")
-
-        user_name = self._clean_all_tag_from_str(bs.findAll("title")[0])
-
-        return user_name.split()[0]
 
 token = "11a6e50ceacffd27faefb555ce9a1db946fc5f79b5d7d22b9571337392dae6c8265a595286eac59f484c7"
 
 vk = vk_api.VkApi(token=token)
+
 
 def write_msg(user_id, random_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message, "random_id": random_id})
@@ -30,24 +27,29 @@ def write_msg(user_id, random_id, message):
 
 longpoll = VkLongPoll(vk)
 
+
 # # Основной цикл
-for event in longpoll.listen():
+def main():
+    for event in longpoll.listen():
 
-    # Если пришло новое сообщение
-    if event.type == VkEventType.MESSAGE_NEW:
+        # Если пришло новое сообщение
+        if event.type == VkEventType.MESSAGE_NEW:
 
-        # Если оно имеет метку для меня( то есть бота)
-        if event.to_me:
+            # Если оно имеет метку для меня( то есть бота)
+            if event.to_me:
 
-            # Сообщение от пользователя
-            request = event.text
+                # Каменная логика ответа
+                if session.query(User).filter(User.user_id == event.user_id).first():
+                    result = ml_part(event.text)
+                    write_msg(event.user_id, event.random_id, result)
+                elif event.text in token_list:
+                    user = User(user_id=event.user_id)
+                    session.add(user)
+                    session.commit()
+                    write_msg(event.user_id, event.random_id, 'Теперь ты авторизован!')
+                else:
+                    write_msg(event.user_id, event.random_id, 'Привет! Я тебя не знаю, пришли мне токен')
 
-            # Каменная логика ответа
-            if request == "привет":
-                write_msg(event.user_id, event.random_id, f"xай")
-                print("dfgdf")
-            elif request == "пока":
-                write_msg(event.user_id, "Пока((")
-            else:
-                write_msg(event.user_id, "Не поняла вашего ответа...")
 
+if __name__ == '__main__':
+    main()
