34b815dcfe3e61368530e6dc85320aee6a24f89b
Anatoly Rubanov <anatolijrubanov@gmail.com>
2019-11-16 20:11:39 +0300

    add metrics

--- vk_bot.py
+++ vk_bot.py
@@ -1,19 +1,32 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-import requests
 from models import Base, User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import threading
+import socket
+from time import sleep
 
 engine = create_engine('sqlite:///bot.db')
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 token_list = ['123']
+req_count = 0
+sock = socket.socket()
+sock.connect(('35.204.44.141', 2003))
 
 
 def ml_part(query_string):
     return "blablabla"
+
+
+def agregate():
+    global req_count
+    sleep(60)
+    sock.send(f'hackathon.team3.backend.cnt {req_count} -1\n'.encode('utf-8'))
+    print('sended')
+    req_count = 0
 
 
 token = "11a6e50ceacffd27faefb555ce9a1db946fc5f79b5d7d22b9571337392dae6c8265a595286eac59f484c7"
@@ -37,6 +50,8 @@ def main():
 
             # Если оно имеет метку для меня( то есть бота)
             if event.to_me:
+                global req_count
+                req_count = req_count + 1
 
                 # Каменная логика ответа
                 if session.query(User).filter(User.user_id == event.user_id).first():
@@ -52,4 +67,6 @@ def main():
 
 
 if __name__ == '__main__':
+    t = threading.Thread(target=agregate)
+    t.start()
     main()
